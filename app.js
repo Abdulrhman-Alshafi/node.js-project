0b9aa66724a00a10007c30412a779c2b776302b9
Abdulrhman-Alshafi <Abdulrahman.Alshafi@optimumpartners.com>
2025-11-18 13:22:56 +0300

    3rd commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
+
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-const { result } = require("lodash");
+const blogRoutes = require("./routes/blogRoutes");
+
 const app = express();
 
 // MongoDB connection URI (never expose passwords in real apps!)
@@ -39,55 +40,8 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blog", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
 
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { title: "Blog Info", blog: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+app.use("/blogs", blogRoutes);
 
 // 404 page - must be last
 app.use((req, res) => {
