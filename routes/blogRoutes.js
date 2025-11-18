6050a329596905dcfe7b38e4691774ffd6b64e7b
Abdulrhman-Alshafi <Abdulrahman.Alshafi@optimumpartners.com>
2025-11-18 18:01:27 +0300

    last

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,10 +4,19 @@ const blogController = require("../controller/blogController");
 const Blog = require("../models/blog");
 const router = express.Router();
 
+//index
 router.get("/", blogController.blog_index);
+
+//all posts
 router.post("/", blogController.blog_create_post);
+
+//form
 router.get("/create", blogController.blog_create_get);
+
+//get post by id
 router.get("/:id", blogController.blog_details);
+
+//delete post by id
 router.delete("/:id", blogController.blog_delete);
 
 module.exports = router;
